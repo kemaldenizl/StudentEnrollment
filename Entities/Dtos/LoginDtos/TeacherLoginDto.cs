06051c931dd39d7c9e8058dc10975665d2c3da6a
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-24 17:26:21 +0300

    Complating Dto classes with implemented from IDto.

---
+++ Entities/Dtos/LoginDtos/TeacherLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos.LoginDtos
+{
+	public class TeacherLoginDto : IDto
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
---
+++ Entities/Dtos/RegisterDtos/StudentRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities.Abstract;
+
+namespace Entities.Dtos.RegisterDtos
+{
+	public class StudentRegisterDto : IDto
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public DateTime EnrollmentDate { get; set; }
+		public string Classroom { get; set; }
+	}
+}
