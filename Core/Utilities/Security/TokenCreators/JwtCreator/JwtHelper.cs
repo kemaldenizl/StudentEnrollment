e0d68748bed24354798f53efbc3a2e9a9e3636fa
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-24 14:36:37 +0300

    Adding ITokenHelper implementation to JwtHelper and adding same generic constraints.

--- Core/Utilities/Security/TokenCreators/JwtCreator/JwtHelper.cs
+++ Core/Utilities/Security/TokenCreators/JwtCreator/JwtHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,9 @@ using System.Threading.Tasks;
 
 namespace Core.Utilities.Security.TokenCreators.JwtCreator
 {
-	public class JwtHelper
+	public class JwtHelper<TUser, TOperationClaim> : ITokenHelper<TUser, TOperationClaim>
+		where TUser : class, IEntity, new()
+		where TOperationClaim : class, IEntity, new()
 	{
 	}
 }
