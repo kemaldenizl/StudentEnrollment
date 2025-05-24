ac39244d578a936f66545115f17e1f3fb002fb94
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-24 21:25:06 +0300

    Inheriting IUserDal interfaces from IEntityRepository

--- DataAccess/Abstract/IAdminDal.cs
+++ DataAccess/Abstract/IAdminDal.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DataAccess.Abstract;
+using Entities.Concrete;
 
 namespace DataAccess.Abstract
 {
-	public interface IAdminDal
+	public interface IAdminDal : IEntityRepository<Admin>
 	{
 	}
 }
