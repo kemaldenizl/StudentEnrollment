9bbd2c91f87c6b4c6268d790eaf5d536b1c52d47
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-24 21:43:50 +0300

    Overriding OnConfiguring method and adding connection string temporarily.

--- DataAccess/Concrete/EntityFramework/Contexts/StudentEnrollmentContext.cs
+++ DataAccess/Concrete/EntityFramework/Contexts/StudentEnrollmentContext.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework.Contexts
 {
-	public class StudentEnrollmentContext
+	public class StudentEnrollmentContext:DbContext
 	{
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer(@"Server=DESKTOP-3C9L670;Database=StudentEnrollment;Trusted_Connection=true;TrustServerCertificate=True");
+			//this connection string is temporarily here. After testing api this connection string will go to appsettings.json file.
+		}
+
 	}
 }
