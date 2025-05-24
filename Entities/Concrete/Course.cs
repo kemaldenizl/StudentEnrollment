dfe71229e2edefb4a76cf4f2105c8d00d543f727
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-24 17:17:13 +0300

    Complating Entity classes with implemented from IEntity.

---
+++ Entities/Concrete/Course.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+	public class Course : IEntity
+	{
+		public int Id { get; set; }
+		public string CourseName { get; set; }
+		public int TeacherId { get; set; }
+	}
+}
---
+++ Entities/Concrete/StudentOperationClaim.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+	public class StudentOperationClaim : IEntity
+	{
+		public int Id { get; set; }
+		public int StudentId { get; set; }
+		public int OperationClaimId { get; set; }
+	}
+}
