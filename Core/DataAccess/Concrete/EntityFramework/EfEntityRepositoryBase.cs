a0b856d616acb778cb8098e9585b8e891644562d
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-24 12:16:24 +0300

    Implementing IEntityRepository interface to EfEntityRepositoryBase class and adding generic constraint

--- Core/DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/Concrete/EntityFramework/EfEntityRepositoryBase.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DataAccess.Abstract;
+using Microsoft.EntityFrameworkCore;
 
 namespace Core.DataAccess.Concrete.EntityFramework
 {
-	public class EfEntityRepositoryBase
+	public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+		where TEntity : class, new()
+		where TContext : DbContext, new()
 	{
 	}
 }
