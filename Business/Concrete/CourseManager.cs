68d3d368c8c5f7a590d740ac0370a88ca294aeb3
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 21:47:44 +0300

    Implementing missing members of CourseService implementation.

--- Business/Concrete/CourseManager.cs
+++ Business/Concrete/CourseManager.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Business.Abstract;
+using Core.Utilities.Results.Abstract;
 using DataAccess.Abstract;
+using Entities.Concrete;
 
 namespace Business.Concrete
 {
@@ -15,6 +17,31 @@ namespace Business.Concrete
 		public CourseManager(ICourseDal courseDal)
 		{
 			_courseDal = courseDal;
+		}
+
+		public IDataResult<List<Course>> GetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IDataResult<Course> GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IResult Add(Course course)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IResult Update(Course course)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IResult Delete(Course course)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
