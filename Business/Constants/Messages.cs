1ef4c8476a99bd2ec7df06a3b3ece176ceae5108
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 12:40:49 +0300

    Creating messages class this class provides api result returns message. This class provides string management.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+	public class Messages
+	{
+		public static string UserNotFound = "User not found.";
+		public static string PasswordError = "Invalid password.";
+		public static string SuccessfulLogin = "Login successful.";
+		public static string UserAlreadyExists = "This user is already exists.";
+		public static string UserRegistered = "User succesfully register.";
+		public static string AccessTokenCreated = "Access token succesfully created.";
+	}
+}
