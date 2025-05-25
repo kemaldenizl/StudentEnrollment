b8a7da9c60ce6089347befc7381450ad41aabeeb
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 13:34:53 +0300

    Starting teacherAuthManager first implementing ITeacherAuthManager than creating prop for teacher and token operations with dependency injection.

--- Business/Concrete/AuthManagers/TeacherAuthManager.cs
+++ Business/Concrete/AuthManagers/TeacherAuthManager.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Abstract.AuthServices;
+using Core.Utilities.Security.TokenCreators;
+using Entities.Concrete;
 
 namespace Business.Concrete.AuthManagers
 {
-	public class TeacherAuthManager
+	public class TeacherAuthManager : ITeacherAuthService
 	{
+		private ITeacherService _teacherService;
+		private ITokenHelper<Teacher, OperationClaim> _tokenHelper;
+
+		public TeacherAuthManager(ITeacherService teacherService, ITokenHelper<Teacher, OperationClaim> tokenHelper)
+		{
+			_teacherService = teacherService;
+			_tokenHelper = tokenHelper;
+		}
 	}
 }
