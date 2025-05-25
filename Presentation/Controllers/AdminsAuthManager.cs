dda9ac8a1ec2392a7c5a2fedad819c7d82167b29
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 20:17:00 +0300

    Complating admin operation.

---
+++ Presentation/Controllers/AdminsAuthManager.cs
@@ -0,0 +1,58 @@
+﻿using Business.Abstract.AuthServices;
+using Entities.Dtos.LoginDtos;
+using Entities.Dtos.RegisterDtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AdminsAuthManager : ControllerBase
+	{
+		private IAdminAuthService _adminAuthService;
+
+		public AdminsAuthManager(IAdminAuthService adminAuthService)
+		{
+			_adminAuthService = adminAuthService;
+		}
+
+		[HttpPost("login")]
+		public ActionResult Login([FromBody] AdminLoginDto adminLoginDto)
+		{
+			var userToLogin = _adminAuthService.Login(adminLoginDto);
+			if (!userToLogin.Success)
+			{
+				return BadRequest(userToLogin.Message);
+			}
+			var result = _adminAuthService.CreateAccessToken(userToLogin.Data);
+
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+			return BadRequest(result.Message);
+		}
+
+		[HttpPost("register")]
+		public ActionResult Register([FromBody] AdminRegisterDto adminRegisterDto)
+		{
+			var userExists = _adminAuthService.UserExists(adminRegisterDto.Email);
+
+			if (!userExists.Success)
+			{
+				return BadRequest(userExists.Message);
+			}
+
+			var registerResult = _adminAuthService.Register(adminRegisterDto);
+			var result = _adminAuthService.CreateAccessToken(registerResult.Data);
+
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+
+			return BadRequest(result.Message);
+		}
+	}
+}
