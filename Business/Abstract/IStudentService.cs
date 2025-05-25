67969c8867292111cf812378d32d7c71acaa74a1
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 12:48:24 +0300

    Creating interfaces for performs auth database operations.

---
+++ Business/Abstract/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+	public interface IStudentService
+	{
+		List<OperationClaim> GetClaims(Student student);
+		void Add(Student student);
+		Student GetByMail(string email);
+	}
+}
---
+++ Business/Abstract/ITeacherService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+	public interface ITeacherService
+	{
+		List<OperationClaim> GetClaims(Teacher teacher);
+		void Add(Teacher teacher);
+		Teacher GetByMail(string email);
+	}
+}
