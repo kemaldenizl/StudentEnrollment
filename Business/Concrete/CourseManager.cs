bd47e00d9fa72bacd30e7dcf543badf6616b11f2
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 21:47:13 +0300

    Creating CourseManager, adding dependency injection of CouseDal.

---
+++ Business/Concrete/CourseManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+	public class CourseManager : ICourseService
+	{
+		public ICourseDal _courseDal;
+
+		public CourseManager(ICourseDal courseDal)
+		{
+			_courseDal = courseDal;
+		}
+	}
+}
