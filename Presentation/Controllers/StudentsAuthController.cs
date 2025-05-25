450d8d5280fd520fe1bf65341e1c5b704d75ce4e
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 18:59:30 +0300

    Complating login operation.

--- Presentation/Controllers/StudentsAuthController.cs
+++ Presentation/Controllers/StudentsAuthController.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract.AuthServices;
+using Entities.Dtos.LoginDtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,6 +15,21 @@ namespace Presentation.Controllers
 			_studentAuthService = studentAuthService;
 		}
 
+		[HttpPost("login")]
+		public ActionResult Login([FromBody] StudentLoginDto studentLoginDto)
+		{
+			var userToLogin = _studentAuthService.Login(studentLoginDto); //Users form information send to login service.
+			if (!userToLogin.Success) //if not success, return BadRequest with the message.
+			{
+				return BadRequest(userToLogin.Message);
+			}
+			var result = _studentAuthService.CreateAccessToken(userToLogin.Data); // if success, create access token for the user.
 
+			if (result.Success) //if access token created successfully, return Ok with the access token.
+			{
+				return Ok(result.Data);
+			}
+			return BadRequest(result.Message); //if access token creation failed, return BadRequest with the message.
+		}
 	}
 }
