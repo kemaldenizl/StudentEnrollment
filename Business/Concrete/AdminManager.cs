ce22bf6d7a7c9820779981cf7ff4eebdf81e6096
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 12:51:25 +0300

    Creating adminManager class. Creating adminDal field for perform database operations. adminDal access provides by dependency injection.

---
+++ Business/Concrete/AdminManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+
+namespace Business.Concrete
+{
+	public class AdminManager : IAdminService
+	{
+		private IAdminDal _adminDal;
+
+		public AdminManager(IAdminDal adminDal)
+		{
+			_adminDal = adminDal;
+		}
+
+	}
+}
