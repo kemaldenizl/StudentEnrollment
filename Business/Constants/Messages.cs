617960ad2b8ce5c7e3e3395f95767ed7c069f925
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 21:53:20 +0300

    Adding message constraints to Messages.cs in business layer.

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -14,5 +14,11 @@ namespace Business.Constants
 		public static string UserAlreadyExists = "This user is already exists.";
 		public static string UserRegistered = "User succesfully register.";
 		public static string AccessTokenCreated = "Access token succesfully created.";
+
+		public static string SuccessGetData = "Data getting successfully.";
+		public static string ErrorGetData = "Error getting data.";
+		public static string SuccessAddData = "Data added successfully.";
+		public static string SuccessUpdateData = "Data updated successfully.";
+		public static string SuccessDeleteData = "Data deleted successfully.";
 	}
 }
