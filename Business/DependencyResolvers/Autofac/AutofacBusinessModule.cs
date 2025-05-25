7adced4323d798db310885c78b809e0e210c46b3
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 19:10:51 +0300

    Creating module class for autofac. Overriding Load method.

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace Business.DependencyResolvers.Autofac
+{
+	public class AutofacBusinessModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+
+		}
+	}
+}
