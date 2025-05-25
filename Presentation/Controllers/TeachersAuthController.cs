29d1c39da78df834b45cbb80cd73fe42d8395967
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 20:14:50 +0300

    Complating teacher operation.

---
+++ Presentation/Controllers/TeachersAuthController.cs
@@ -0,0 +1,57 @@
+﻿using Business.Abstract.AuthServices;
+using Entities.Dtos.LoginDtos;
+using Entities.Dtos.RegisterDtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TeachersAuthController : ControllerBase
+	{
+		private ITeacherAuthService _teacherAuthService;
+		public TeachersAuthController(ITeacherAuthService teacherAuthService)
+		{
+			_teacherAuthService = teacherAuthService;
+		}
+
+		[HttpPost("login")]
+		public ActionResult Login([FromBody] TeacherLoginDto teacherLoginDto)
+		{
+			var userToLogin = _teacherAuthService.Login(teacherLoginDto);
+			if (!userToLogin.Success)
+			{
+				return BadRequest(userToLogin.Message);
+			}
+			var result = _teacherAuthService.CreateAccessToken(userToLogin.Data);
+
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+			return BadRequest(result.Message);
+		}
+
+		[HttpPost("register")]
+		public ActionResult Register([FromBody] TeacherRegisterDto teacherRegisterDto)
+		{
+			var userExists = _teacherAuthService.UserExists(teacherRegisterDto.Email);
+
+			if (!userExists.Success)
+			{
+				return BadRequest(userExists.Message);
+			}
+
+			var registerResult = _teacherAuthService.Register(teacherRegisterDto);
+			var result = _teacherAuthService.CreateAccessToken(registerResult.Data);
+
+			if (result.Success)
+			{
+				return Ok(result.Data);
+			}
+
+			return BadRequest(result.Message);
+		}
+	}
+}
