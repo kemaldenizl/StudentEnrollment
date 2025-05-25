35aae4eb313523abc6b82f26588865e047d634c1
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 13:45:27 +0300

    Complating login operation for teacher.

--- Business/Concrete/AuthManagers/TeacherAuthManager.cs
+++ Business/Concrete/AuthManagers/TeacherAuthManager.cs
@@ -8,9 +8,11 @@ using Business.Abstract.AuthServices;
 using Business.Constants;
 using Core.Utilities.Results.Abstract;
 using Core.Utilities.Results.Concrete.DataResultTypes;
+using Core.Utilities.Security.Hashing;
 using Core.Utilities.Security.TokenCreators;
 using Core.Utilities.Security.TokenEntities;
 using Entities.Concrete;
+using Entities.Dtos.LoginDtos;
 
 namespace Business.Concrete.AuthManagers
 {
@@ -31,6 +33,23 @@ namespace Business.Concrete.AuthManagers
 			var accessToken = _tokenHelper.CreateToken(teacher, claims);
 
 			return new SuccessDataResult<AccessToken>(accessToken, Messages.AccessTokenCreated);
+		}
+
+		public IDataResult<Teacher> Login(TeacherLoginDto teacherLoginDto)
+		{
+			var teacherToCheck = _teacherService.GetByMail(teacherLoginDto.Email); //first check if the teacher exists
+
+			if (teacherToCheck == null) //if teacher not found return error
+			{
+				return new ErrorDataResult<Teacher>(Messages.UserNotFound);
+			}
+
+			if (!HashingHelper.VerifyPasswordHash(teacherLoginDto.Password, teacherToCheck.PasswordHash,teacherToCheck.PasswordSalt)) //check if the password is false
+			{
+				return new ErrorDataResult<Teacher>(Messages.PasswordError);
+			}
+
+			return new SuccessDataResult<Teacher>(teacherToCheck, Messages.SuccessfulLogin); //Success login operation return teacher entity.
 		}
 	}
 }
