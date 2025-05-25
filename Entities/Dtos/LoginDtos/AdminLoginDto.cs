545ea5fa074d4bd75ff86722535a8a26c9172e41
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 19:47:05 +0300

    Adding xml serialization to dtos for xml support.

--- Entities/Dtos/LoginDtos/AdminLoginDto.cs
+++ Entities/Dtos/LoginDtos/AdminLoginDto.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Core.Entities.Abstract;
 
 namespace Entities.Dtos.LoginDtos
 {
+	[XmlRoot(ElementName = "AdminLoginDto")]
 	public class AdminLoginDto : IDto
 	{
+		[XmlElement(ElementName = "Email")]
 		public string Email { get; set; }
+		[XmlElement(ElementName = "Password")]
 		public string Password { get; set; }
 	}
 }
--- Entities/Dtos/LoginDtos/TeacherLoginDto.cs
+++ Entities/Dtos/LoginDtos/TeacherLoginDto.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Entities.Dtos.LoginDtos
 {
+	[XmlRoot(ElementName = "TeacherLoginDto")]
 	public class TeacherLoginDto : IDto
 	{
+		[XmlElement(ElementName = "Email")]
 		public string Email { get; set; }
+		[XmlElement(ElementName = "Password")]
 		public string Password { get; set; }
 	}
 }
