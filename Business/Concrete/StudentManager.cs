71e3f9cd81b5a11507dce4e43706dc5b94ef1b4f
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 12:59:59 +0300

    Complating same methods in Student and Teacher manager.

---
+++ Business/Concrete/StudentManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+	public class StudentManager : IStudentService
+	{
+		private IStudentDal _studentDal;
+
+		public StudentManager(IStudentDal studentDal)
+		{
+			_studentDal = studentDal;
+		}
+
+		public List<OperationClaim> GetClaims(Student student)
+		{
+			return _studentDal.GetClaims(student);
+		}
+
+		public void Add(Student student)
+		{
+			_studentDal.Add(student);
+		}
+
+		public Student GetByMail(string email)
+		{
+			return _studentDal.Get(s => s.Email == email);
+		}
+	}
+}
---
+++ Business/Concrete/TeacherManager.cs
@@ -0,0 +1,35 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+
+namespace Business.Concrete
+{
+	public class TeacherManager : ITeacherService
+	{
+		private ITeacherDal _teacherDal;
+
+		public TeacherManager(ITeacherDal teacherDal)
+		{
+			_teacherDal = teacherDal;
+		}
+		public List<OperationClaim> GetClaims(Teacher teacher)
+		{
+			return _teacherDal.GetClaims(teacher);
+		}
+
+		public void Add(Teacher teacher)
+		{
+			_teacherDal.Add(teacher);
+		}
+
+		public Teacher GetByMail(string email)
+		{
+			return _teacherDal.Get(t => t.Email == email);
+		}
+	}
+}
