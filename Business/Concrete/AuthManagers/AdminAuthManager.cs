1805c956a436b445e0c28f6eb9b5c21e8bd0432a
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 13:57:36 +0300

    Starting AdminAuthManager, adding dependency injections.

--- Business/Concrete/AuthManagers/AdminAuthManager.cs
+++ Business/Concrete/AuthManagers/AdminAuthManager.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Abstract.AuthServices;
+using Core.Utilities.Security.TokenCreators;
+using Entities.Concrete;
 
 namespace Business.Concrete.AuthManagers
 {
-	public class AdminAuthManager
+	public class AdminAuthManager : IAdminAuthService
 	{
+		private IAdminService _adminService;
+		private ITokenHelper<Admin, OperationClaim> _tokenHelper;
+
+		public AdminAuthManager(IAdminService adminService, ITokenHelper<Admin, OperationClaim> tokenHelper)
+		{
+			_adminService = adminService;
+			_tokenHelper = tokenHelper;
+		}
+
 	}
 }
