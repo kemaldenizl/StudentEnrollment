3fd9d5d46bccd9a06ead564dd9b08c99a143d6f9
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 21:55:10 +0300

    Starting to coding course controller first getting courseService with dependency injection.

---
+++ Presentation/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CoursesController : ControllerBase
+	{
+		private ICourseService _courseService;
+		public CoursesController(ICourseService courseService)
+		{
+			_courseService = courseService;
+		}
+	}
+}
