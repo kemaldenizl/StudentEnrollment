ab2fb998fb8d9f942b9733407416c58d965d7edc
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 12:04:24 +0300

    Complating EfTeacherDal and GetClaim method.

---
+++ DataAccess/Concrete/EntityFramework/EfTeacherDal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataAccess.Concrete.EntityFramework;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
+using Entities.Concrete;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+	public class EfTeacherDal : EfEntityRepositoryBase<Teacher, StudentEnrollmentContext>, ITeacherDal
+	{
+		public List<OperationClaim> GetClaims(Teacher teacher)
+		{
+			using (var context = new StudentEnrollmentContext())
+			{
+				var result = from operationClaim in context.OperationClaims
+					join TeacherOperationClaim in context.TeacherOperationClaims
+						on operationClaim.Id equals TeacherOperationClaim.OperationClaimId
+					where TeacherOperationClaim.TeacherId == teacher.Id
+					select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+
+				return result.ToList();
+			}
+		}
+	}
+}
