33e37b9d4b9a3814871be743ea1e676f9edf459c
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-25 13:16:23 +0300

    Creating AuthServices. Creating TeacherAuthService interface for auth operation.

---
+++ Business/Abstract/AuthServices/ITeacherAuthService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Security.TokenEntities;
+using Entities.Concrete;
+using Entities.Dtos.LoginDtos;
+using Entities.Dtos.RegisterDtos;
+
+namespace Business.Abstract.AuthServices
+{
+	public interface ITeacherAuthService
+	{
+		IDataResult<Teacher> Register(TeacherRegisterDto teacherRegisterDto);
+		IDataResult<Teacher> Login(TeacherLoginDto teacherLoginDto);
+		IResult UserExists(string email);
+		IDataResult<AccessToken> CreateAccessToken(Teacher teacher);
+	}
+}
