24f5350ba5393adf7cf77e550d5b6be9eed26f83
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-27 12:22:58 +0300

    Complating enrollmentController.

---
+++ Presentation/Controllers/EnrollmentsController.cs
@@ -0,0 +1,74 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EnrollmentsController : ControllerBase
+	{
+		private IEnrollmentService _enrollmentService;
+
+		public EnrollmentsController(IEnrollmentService enrollmentService)
+		{
+			_enrollmentService = enrollmentService;
+		}
+
+		[HttpGet()]
+		public IActionResult GetAll()
+		{
+			var result = _enrollmentService.GetAll();
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			var result = _enrollmentService.GetById(id);
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+
+		[HttpPost("add")]
+		public IActionResult Add(Enrollment enrollment)
+		{
+			var result = _enrollmentService.Add(enrollment);
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+
+		[HttpPut("update")]
+		public IActionResult Update(Enrollment enrollment)
+		{
+			var result = _enrollmentService.Update(enrollment);
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+
+		[HttpDelete("delete")]
+		public IActionResult Delete(Enrollment enrollment)
+		{
+			var result = _enrollmentService.Delete(enrollment);
+			if (result.Success)
+			{
+				return Ok(result);
+			}
+			return BadRequest(result);
+		}
+	}
+}
