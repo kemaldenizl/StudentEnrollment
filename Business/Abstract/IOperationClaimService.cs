7433e67e2e4b680e8e3e7371c0b5ef059ffaadcc
Kemal Deniz Lağap <Kemal@DESKTOP-3C9L670>
2025-05-27 12:47:29 +0300

    Complating Business layer for operationClaim.

---
+++ Business/Abstract/IOperationClaimService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+	public interface IOperationClaimService
+	{
+		IDataResult<List<OperationClaim>> GetAll();
+		IDataResult<OperationClaim> GetById(int id);
+		IResult Add(OperationClaim operationClaim);
+		IResult Update(OperationClaim operationClaim);
+		IResult Delete(OperationClaim operationClaim);
+	}
+}
---
+++ Business/Concrete/OperationClaimManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Results.Concrete.DataResultTypes;
+using Core.Utilities.Results.Concrete.ResultTypes;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+	public class OperationClaimManager : IOperationClaimService
+	{
+		public IOperationClaimDal _operationClaimDal;
+
+		public OperationClaimManager(IOperationClaimDal operationClaimDal)
+		{
+			_operationClaimDal = operationClaimDal;
+		}
+
+		public IDataResult<List<OperationClaim>> GetAll()
+		{
+			var result = _operationClaimDal.GetAll();
+
+			if (result == null)
+			{
+				return new ErrorDataResult<List<OperationClaim>>(Messages.ErrorGetData);
+			}
+
+			return new SuccessDataResult<List<OperationClaim>>(result, Messages.SuccessGetData);
+		}
+
+		public IDataResult<OperationClaim> GetById(int id)
+		{
+			var result = _operationClaimDal.Get(o => o.Id == id);
+
+			if (result == null)
+			{
+				return new ErrorDataResult<OperationClaim>(Messages.ErrorGetData);
+			}
+
+			return new SuccessDataResult<OperationClaim>(result, Messages.SuccessGetData);
+		}
+
+		public IResult Add(OperationClaim operationClaim)
+		{
+			_operationClaimDal.Add(operationClaim);
+			return new SuccessResult(Messages.SuccessAddData);
+		}
+
+		public IResult Update(OperationClaim operationClaim)
+		{
+			_operationClaimDal.Update(operationClaim);
+			return new SuccessResult(Messages.SuccessUpdateData);
+		}
+
+		public IResult Delete(OperationClaim operationClaim)
+		{
+			_operationClaimDal.Delete(operationClaim);
+			return new SuccessResult(Messages.SuccessDeleteData);
+		}
+	}
+}
